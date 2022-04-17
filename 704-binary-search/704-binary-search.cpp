176e705fa007cf2de83405c3847ffe41469264b2
Karan Banjade <40836009+KaranBanjade@users.noreply.github.com>
2022-04-17 23:47:49 +0530

    Time: 41 ms (72.81%), Space: 27.6 MB (12.71%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -3,7 +3,6 @@ public:
     int search(vector<int>& nums, int target) {
         int beg = 0;
         int end = nums.size() - 1;
-        int mid = 0;
        while(end>=beg){
            int mid = (beg + end)/2;
            if(nums[mid] > target)
